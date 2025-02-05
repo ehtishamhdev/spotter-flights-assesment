e53aa68c71e54d5b7630bcf8372ee2c88dd72803
ehtisham <ehtishamh.dev@gmail.com>
2025-02-05 19:11:30 +0500

    Added toastify to diplay error notifications

--- src/services/baseService.ts
+++ src/services/baseService.ts
@@ -1,21 +1,28 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from "axios";
+import { toast } from "react-toastify";
 
 const apiService = (): AxiosInstance => {
   const defaultOptions = {
-    baseURL:  `https://${import.meta.env.VITE_RAPID_API_HOST}/api/v1/flights`,
+    baseURL: `https://${import.meta.env.VITE_RAPID_API_HOST}/api/v1/flights`,
     headers: {
-        "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY as string,
-        "X-RapidAPI-Host": import.meta.env.VITE_RAPID_API_HOST as string,
-      },
+      "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY as string,
+      "X-RapidAPI-Host": import.meta.env.VITE_RAPID_API_HOST as string,
+    },
   };
   const instance = axios.create(defaultOptions);
-
 
   instance.interceptors.response.use(
     (response) => response,
     (err) => {
- 
-
+      if (err.response) {
+        toast.error(
+          `Error: ${err.response.status} - ${err.response.data.message}`
+        );
+      } else if (err.request) {
+        toast.error("Error: No response received from server.");
+      } else {
+        toast.error(`Error: ${err.message}`);
+      }
       return Promise.reject(err);
     }
   );
